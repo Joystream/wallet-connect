e088779e6e35f8a54ddc720cd7aebb9a95775cfb
attemka <attemka@gmail.com>
2024-02-01 22:15:40 +0530

    include joystream chain in walletconnect config

--- examples/react-headless/src/components/App.tsx
+++ examples/react-headless/src/components/App.tsx
@@ -1,11 +1,10 @@
 import { PolkadotWalletsContextProvider } from '@polkadot-onboard/react'
 import { WalletAggregator } from '@polkadot-onboard/core'
 import { InjectedWalletProvider } from '@polkadot-onboard/injected-wallets'
-import { WalletConnectProvider, walletConnectParams } from '@polkadot-onboard/wallet-connect'
+import { WalletConnectProvider, WalletConnectConfiguration } from '@polkadot-onboard/wallet-connect'
 import { useState } from 'react'
 
 import Wallets from './Wallets'
-import { WalletConnectConfiguration } from '@polkadot-onboard/wallet-connect/dist';
 
 const APP_NAME = 'Joystream Demo app'
 
@@ -20,8 +19,8 @@ const App = () => {
       url: '#',
       icons: ['https://walletconnect.com/walletconnect-logo.png'],
     },
-    chainIds: ['polkadot:e143f23803ac50e8f6f8e62695d1ce9e', 'polkadot:91b171bb158e2d3848fa23a9f1c25182'],
-    optionalChainIds: ['polkadot:67f9723393ef76214df0118c34bbbd3d', 'polkadot:7c34d42fc815d392057c78b49f2755c7'],
+    chainIds: ['polkadot:6b5e488e0fa8f9821110d5c13f4c468a'],
+    optionalChainIds: [],
     onSessionDelete: () => {
       // do something when session is removed
     },
